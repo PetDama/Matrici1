a2d86172325fa492721c878bbfa54a4e0785f184
Damaris Florina Petrila <damaris.petrila@emmanuel.ro>
2021-12-02 16:55:29 +0200

    Matrici1

--- Matrici1/Matrici1/Program.cs
+++ Matrici1/Matrici1/Program.cs
@@ -6,7 +6,37 @@ namespace Matrici1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nr linii:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] matrice = new int[N, N];
+
+            bool esteIdentitate = true;
+
+            for (int row = 0; row < matrice.GetLength(0); row++)
+            
+                for (int col = 0; col < matrice.GetLength(1); col++)
+                {
+                    Console.Write($"[{row},{col}]:");
+                    matrice[row, col] = Convert.ToInt32(Console.ReadLine());
+
+                    if (row != col)
+                    {
+                        if (matrice[row, col] != 0)
+                        {
+                            esteIdentitate = false;
+                        }
+                    }
+                    else if (matrice[row, col] != 1)
+                    {
+                        esteIdentitate = false;
+                    }
+                }
+                if (esteIdentitate == true)
+                    Console.WriteLine("Este matrice identitate!");
+                else
+                    Console.WriteLine("Nu este matrice identitate");
+
+            
         }
     }
 }
